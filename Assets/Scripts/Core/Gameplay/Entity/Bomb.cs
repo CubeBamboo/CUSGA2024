3c493ba221a07e7953e5564540b80458b9bfe691
dogdie233 <dogdie233@163.com>
2024-03-29 22:48:22 +0800

    #23

--- Assets/Scripts/Core/Gameplay/Entity/Bomb.cs
+++ Assets/Scripts/Core/Gameplay/Entity/Bomb.cs
@@ -19,8 +19,6 @@ namespace Shuile.Gameplay.Entity
             var grid = LevelGrid.Instance.grid;
             grid.Get(gridPosition)?.GetComponent<IAttackable>()?.OnAttack(attackPoint);
             var player = GameplayService.Interface.Get<PlayerController>();
-            if (grid.WorldToCell(player.transform.position) == gridPosition)  // 因为现在玩家不在grid里，所以要这么做
-                player.OnAttack(attackPoint);
 
             // play anim
             transform.GetChild(0).transform.DOScale(6f, 0.2f).From().OnComplete(() => Destroy(this.gameObject));
