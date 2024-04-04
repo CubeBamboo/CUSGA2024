80e665f805c28f58315c0adc333bb93b025b7a6f
dogdie233 <dogdie233@163.com>
2024-04-05 03:51:53 +0800

    add: 乱写了一个TriggerTrap

---
+++ Assets/Scripts/Core/Gameplay/TriggerTrap.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Shuile.Gameplay
+{
+    public abstract class TriggerTrap : MonoBehaviour, IJudgeable
+    {
+        protected HashSet<Vector3Int> colliders = new();
+
+        protected virtual void Awake()
+        {
+            // Register OnEntityMove
+        }
+
+        public void Judge(int frame, bool force)
+        {
+            Recheck();
+        }
+
+        // Call from outside
+        private void OnEntityMove(GameObject other, Vector3Int from, Vector3Int to)
+        {
+            var beforeIn = colliders.Contains(from);
+            var nowIn = colliders.Contains(to);
+            if (!(beforeIn ^ nowIn))
+                return;
+
+            if (beforeIn)
+                OnExit(other);
+            else
+                OnEnter(other);
+        }
+
+        public abstract void Recheck();
+
+        public abstract void OnEnter(GameObject other);
+        public abstract void OnExit(GameObject other);
+    }
+}
---
+++ Assets/Scripts/Utils/HashSetExtension.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Shuile
+{
+    public static class HashSetExtension
+    {
+        public static void AddRange<T>(this HashSet<T> set, params T[] items)
+        {
+            foreach (var item in items)
+                set.Add(item);
+        }
+    }
+}
