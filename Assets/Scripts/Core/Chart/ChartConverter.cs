1b06230b99d7589acd4c8984d5d47981d18826b8
CubeBamboo <repotak032@163.com>
2024-04-05 13:38:41 +0800

    add: 修改player prefab，[WIP]谱面读取

--- Assets/Scripts/Core/Chart/ChartConverter.cs
+++ Assets/Scripts/Core/Chart/ChartConverter.cs
@@ -1,155 +1,34 @@
-//[WIP]
+// [WIP]
 
-using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Shuile.Rhythm
 {
-    public class ChartConverter
+    public static class ChartConverter
     {
+        public static Chart_MC.Root LoadChart(string chartName)
+        {
+            Chart_MC.Root root = null;
+            var op = Addressables.LoadAssetAsync<TextAsset>($"Assets/Chart/{chartName}");
+            var res = op.WaitForCompletion();
+            root = JsonUtility.FromJson<Chart_MC.Root>(res.text);
 
+            return root;
+        }
+
+        public static string Chart2Text(Chart_MC.Root chart)
+        {
+            string res = JsonUtility.ToJson(chart);
+            return res;
+        }
     }
 
-    #region Chart_MC
-
-    public class Song
+    public class FooChart
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public string title { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string artist { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int id { get; set; }
+        public static void Do()
+        {
+            ChartConverter.LoadChart("testchart.json");
+        }
     }
-
-    public class Mode_ext
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public int column { get; set; }
-    }
-
-    public class Meta
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public int ver { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string creator { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string background { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public string version { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int id { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int mode { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int time { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public Song song { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public Mode_ext mode_ext { get; set; }
-    }
-
-    public class TimeItem
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public List<int> beat { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int bpm { get; set; }
-    }
-
-    public class NoteItem
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public List<int> beat { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int column { get; set; }
-    }
-
-    public class Test
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public int divide { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int speed { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int save { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int @lock { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int edit_mode { get; set; }
-    }
-
-    public class Extra
-    {
-        /// <summary>
-        /// user settings
-        /// </summary>
-        public Test test { get; set; }
-    }
-
-    public class Root
-    {
-        /// <summary>
-        /// record the chart's information (song name, mode, length...)
-        /// </summary>
-        public Meta meta { get; set; }
-        /// <summary>
-        /// timing settings
-        /// </summary>
-        public List<TimeItem> time { get; set; }
-        /// <summary>
-        /// notes
-        /// </summary>
-        public List<NoteItem> note { get; set; }
-        /// <summary>
-        /// other (user settings)
-        /// </summary>
-        public Extra extra { get; set; }
-    }
-
-    #endregion
 }
