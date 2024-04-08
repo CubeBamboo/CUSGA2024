de26db702ef48a96ee67e81aaaf65d5b66293c09
CubeBamboo <repotak032@163.com>
2024-04-08 23:04:12 +0800

    chore: 临时美术素材拼差不多了

--- Assets/Scripts/Core/Gameplay/Entity/States/SpawnState.cs
+++ Assets/Scripts/Core/Gameplay/Entity/States/SpawnState.cs
@@ -12,13 +12,14 @@ namespace Shuile.Gameplay.Entity.States
 
         public override void Judge()
         {
-            entity.transform.GetChild(0).DOScale(1f, 0.3f).SetEase(Ease.OutBounce);
+            //entity.transform.GetChild(0).DOScale(0f, 0.3f).From().SetEase(Ease.OutBounce);
             entity.GotoState(EntityStateType.Idle);
         }
 
         public override void EnterState()
         {
-            entity.transform.GetChild(0).localScale = Vector3.zero;
+            //entity.transform.GetChild(0).localScale = Vector3.zero;
+            entity.transform.GetChild(0).DOScale(0f, 0.3f).From().SetEase(Ease.OutBounce);
         }
 
         public override void ExitState() { }
