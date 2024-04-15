9e90fc080675b15d6cc053cc2ae6bde52ea14eea
CubeBamboo <repotak032@163.com>
2024-04-15 14:57:28 +0800

    refactor: 整理文件夹之Shuile.Utils

--- Assets/Scripts/Shuile.Utils/Framework/Audio/BaseAudioCtrl.cs
+++ Assets/Scripts/Shuile.Utils/Framework/Audio/BaseAudioCtrl.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 namespace Shuile
 {
     /// <summary>
     /// [WIP]
     /// </summary>
-    public class BaseAudioCtrl<TId> : CSharpHungrySingletons<BaseAudioCtrl<TId>>
+    /*public class BaseAudioCtrl<TId> : CSharpHungrySingletons<BaseAudioCtrl<TId>>
     {
         protected Dictionary<TId, AudioClip> mContainer;
 
@@ -26,5 +25,5 @@ namespace Shuile
         }
 
         public AudioClip Get(TId id) => mContainer[id];
-    }
+    }*/
 }
