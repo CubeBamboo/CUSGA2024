9eebcba8bd8cc4c89716557f00dea19cbf6cce8a
dogdie233 <dogdie233@163.com>
2024-05-08 02:40:23 +0800

    feat: 关卡内退出UI

---
+++ Assets/Scripts/Gameplay/GameplayOperationInput.cs
@@ -0,0 +1,35 @@
+using Shuile.UI;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Shuile.Gameplay
+{
+    public class GameplayOperationInput : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput input;
+        [SerializeField] private GameplayExitPanel exitPanel;
+
+        private void Awake()
+        {
+            input.onActionTriggered += OnActionTrigger;
+        }
+
+        private void OnDestroy()
+        {
+            input.onActionTriggered -= OnActionTrigger;
+        }
+
+        private void OnActionTrigger(InputAction.CallbackContext context)
+        {
+            if (context.action.name == "Exit")
+            {
+                Debug.Log(context.action.phase);
+                if (context.action.phase == InputActionPhase.Started)
+                    exitPanel.Show();
+                else if (context.action.phase == InputActionPhase.Canceled)
+                    exitPanel.Hide();
+            }
+        }
+    }
+}
